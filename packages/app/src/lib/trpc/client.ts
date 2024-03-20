0d3424ce8246c3e559c9db521e6dd5be5cd41b91
rharkor <louis@huort.com>
2024-03-20 18:49:11 +0100

    fix: format

--- packages/app/src/lib/trpc/client.ts
+++ packages/app/src/lib/trpc/client.ts
@@ -8,34 +8,33 @@ import { getUrl } from "./utils"
 
 export const trpc = createTRPCReact<AppRouter>({})
 
-
 export const trpcClient = trpc.createClient({
-    transformer: SuperJSON,
-    links: [
-        // adds pretty logs to your console in development and logs errors in production
-        loggerLink({
-            enabled: (opts) =>
-                (process.env.NODE_ENV === "development" && typeof window !== "undefined") ||
-                (opts.direction === "down" && opts.result instanceof Error),
-        }),
-        splitLink({
-            condition(op) {
-                return op.type === "subscription"
-            },
-            true: (() => {
-                const wsClient = createWSClient({
-                    url: env.NEXT_PUBLIC_WS_URL || "ws://localhost:3001",
-                })
-                return wsLink({
-                    client: wsClient,
-                })
-            })(),
-            false: httpBatchLink({
-                url: getUrl(),
-            }),
-        }),
-        httpBatchLink({
-            url: getUrl(),
-        }),
-    ],
+  transformer: SuperJSON,
+  links: [
+    // adds pretty logs to your console in development and logs errors in production
+    loggerLink({
+      enabled: (opts) =>
+        (process.env.NODE_ENV === "development" && typeof window !== "undefined") ||
+        (opts.direction === "down" && opts.result instanceof Error),
+    }),
+    splitLink({
+      condition(op) {
+        return op.type === "subscription"
+      },
+      true: (() => {
+        const wsClient = createWSClient({
+          url: env.NEXT_PUBLIC_WS_URL || "ws://localhost:3001",
+        })
+        return wsLink({
+          client: wsClient,
+        })
+      })(),
+      false: httpBatchLink({
+        url: getUrl(),
+      }),
+    }),
+    httpBatchLink({
+      url: getUrl(),
+    }),
+  ],
 })
